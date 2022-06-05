48cd255fb43b157396f4849aa911f99e5c6197cc
Pablo Lafontaine <plafontaine@ryerson.ca>
2022-06-04 20:34:59 -0400

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config, env) => {
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+      stream: require.resolve("stream-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+    });
+    config.resolve.fallback = fallback;
+    config.ignoreWarnings = [/Failed to parse source map/];  // gets rid of a billion source map warnings
+    return config;
+  },
+};
